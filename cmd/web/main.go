06213cd72e40012768033821313db01b3150d588
Justin Thurman <justin.thurman@bevy.com>
2024-11-12 09:57:37 -0500

    Add structured logging to `main.go`

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"flag"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
 )
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{AddSource: true}))
 
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
@@ -22,8 +26,9 @@ func main() {
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
-	log.Printf("starting server on %s", *addr)
+	logger.Info("starting server", slog.String("addr", *addr))
 
 	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	logger.Error(err.Error())
+	os.Exit(1)
 }
